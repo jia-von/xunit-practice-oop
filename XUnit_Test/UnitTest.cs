9929abf22f18b5a6a33ddc036e30dd774fc99ad8
Jia <jiavon@ualberta.ca>
2020-09-10 16:29:01 -0600

    unit test: Remove a single item from the order (fact). success

--- XUnit_Test/UnitTest.cs
+++ XUnit_Test/UnitTest.cs
@@ -33,5 +33,33 @@ namespace XUnit_Test
             FoodItem food = (FoodItem)newOrder.Items[0];
             Assert.Equal(FoodItem.TypeValue.Drink, food.Type);
         }
+
+        [Fact]
+        public void Add_Combo_Test()// Add a Combo to the order (fact). //add combo based on object combo
+        {
+            Order newOrder = new Order();
+            newOrder.AddItem(new Combo());
+            Assert.Equal(typeof(Combo),newOrder.Items[0].GetType());
+        }
+
+        [Fact]
+        public void Add_ComboItems_Test()// Add a Combo to the order (fact). based on items
+        {
+            Order newOrder = new Order();
+            newOrder.AddItem(new FoodItem() { Type = FoodItem.TypeValue.ChickenStrips, Price = 3.00 });
+            newOrder.AddItem(new FoodItem() { Type = FoodItem.TypeValue.FrenchFries, Price = 2.00 });
+            newOrder.AddItem(new FoodItem() { Type = FoodItem.TypeValue.Drink, Price = 1.00 });
+            Assert.Equal(typeof(Combo), newOrder.Items[0].GetType());
+        }
+
+        [Fact]
+        public void Remove_Single_Item()//Remove a single item from the order (fact).
+        {
+            Order newOrder = new Order();
+            newOrder.AddItem(new FoodItem() { Type = FoodItem.TypeValue.ChickenStrips, Price = 3.00 });
+            newOrder.AddItem(new FoodItem() { Type = FoodItem.TypeValue.FrenchFries, Price = 2.00 });
+            newOrder.RemoveItem();
+            Assert.Equal(3, newOrder.Total);  //check using the price of the item
+        }
     }
 }
